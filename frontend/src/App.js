33414d7deadcf19c1bf53fccf17ea67f7732e828
Julia Borlase <zqj5293@aut.ac.nz>
2022-07-29 13:08:29 +1200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to CISE - the home of learning and fun</h1>
+    </div>
+  );
+}
+
+export default App;
